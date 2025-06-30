9e3c07408683053c2e9e4e379110adcd79d15465
Meryl Marasigan <meryl0729@gmail.com>
2025-06-29 17:05:18 -0700

    implemented adding adding user input to database and displaying it on frontend

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,45 @@
-import react from 'react'
+import react, { useState } from 'react'
 import '../styles/Input.css'
+import supabase from '../config/supabaseClient'
+import { useNavigate } from 'react-router-dom';
+
 const Input = () => {
+    const [title, setTitle] =  useState('')
+    const [formError, setFormError] = useState(null)
+    const navigate = useNavigate();
+
+    const handleSubmit = async(e) => {
+
+        const {data,error} = await supabase.from('task').insert([{title}]).select()
+
+        if(error){
+            console.log(error)
+            setFormError('Please fill in all fields correctly')
+        }
+
+        if(data){
+            console.log(`title ${title} was added to the database`);
+            setFormError(null)
+        }
+
+        navigate('/')
+    } 
     return (
         <div className='input'>
-            <input type='text' placeholder='Enter Task' className='text-bar'>
-            </input>
-            <button>Submit</button>
+            <form onSubmit={handleSubmit}>
+
+                <input type='text'
+                placeholder='Enter title'
+                className='text-bar'
+                value={title}
+                onChange={(event) => {setTitle(event.target.value)}}
+                required
+                >
+                </input>
+                <button>Submit</button>
+
+            </form>
+           
         </div>
     );
 }
